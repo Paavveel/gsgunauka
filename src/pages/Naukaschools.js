1204bea081dddc94b3c2e8bc45155d5222441bbd
Paavveel <pa.tomilin@gmail.com>
2022-01-21 18:42:20 +0300

    some fix

--- src/pages/Naukaschools.js
+++ src/pages/Naukaschools.js
@@ -1,42 +1,7 @@
 import React from 'react';
-// import Swiper core and required modules
-import { Navigation, Pagination, A11y, Autoplay } from 'swiper';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/autoplay';
-
-import vestnik from '../img/vestnik.jpg';
 
 function Naukaschools() {
-  return (
-    <div className='main-page'>
-      <Swiper
-        // install Swiper modules
-        modules={[Navigation, Pagination, Autoplay, A11y]}
-        loop={true}
-        slidesPerView={1}
-        speed={1000}
-        // autoplay={{
-        //   delay: 5000,
-        //   disableOnInteraction: false,
-        // }}
-        spaceBetween={30}
-        navigation={true}
-        pagination={{
-          type: 'fraction',
-        }}
-      >
-        <SwiperSlide>
-          <img src={vestnik} alt='vestnik' className='slide' />
-        </SwiperSlide>
-      </Swiper>
-    </div>
-  );
+  return <div className='main-page'></div>;
 }
 
 export default Naukaschools;
