f5cc516b7e98d8a91d91b3f2847011e9b9a69a0e
Paavveel <pa.tomilin@gmail.com>
2022-03-23 09:25:33 +0300

    add Achiev and Naukaschool

--- src/pages/Achievements.js
+++ src/pages/Achievements.js
@@ -1,11 +1,120 @@
 import React from 'react';
+// import Swiper core and required modules
+import { Navigation, Pagination, A11y, Autoplay } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+
+import img1 from '../img/Achievements/1.jpg';
+import img2 from '../img/Achievements/2.jpg';
+import img3 from '../img/Achievements/3.jpg';
+import img4 from '../img/Achievements/4.jpg';
+import img5 from '../img/Achievements/5.jpg';
+import img6 from '../img/Achievements/6.jpg';
+import img7 from '../img/Achievements/7.jpg';
+import img8 from '../img/Achievements/8.jpg';
+import img9 from '../img/Achievements/9.jpg';
+import img10 from '../img/Achievements/10.jpg';
+import img11 from '../img/Achievements/11.jpg';
+import img12 from '../img/Achievements/12.jpg';
+import img13 from '../img/Achievements/13.jpg';
+import img14 from '../img/Achievements/14.jpg';
+import img15 from '../img/Achievements/15.jpg';
+import img16 from '../img/Achievements/16.jpg';
+import img17 from '../img/Achievements/17.jpg';
+import img18 from '../img/Achievements/18.jpg';
+import img19 from '../img/Achievements/19.jpg';
+import img20 from '../img/Achievements/20.jpg';
+import img21 from '../img/Achievements/21.jpg';
 
 function Achievements() {
   return (
-    <div className='main-page main-container page-titles'>
-      <a href='https://disk.yandex.ru/d/tMRhy07KYwlWjw/11.03.22/%D0%9D%D0%B0%D1%88%D0%B8%20%D0%B4%D0%BE%D1%81%D1%82%D0%B8%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F'>
-        Достижения
-      </a>
+    <div className='main-page'>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Autoplay, A11y]}
+        loop={true}
+        slidesPerView={1}
+        speed={1000}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: true,
+        }}
+        spaceBetween={30}
+        navigation={true}
+        pagination={{
+          type: 'fraction',
+        }}
+      >
+        <SwiperSlide>
+          <img src={img1} alt='img1' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img2} alt='img2' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img3} alt='img3' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img4} alt='img4' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img5} alt='img5' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img6} alt='img6' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img7} alt='img7' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img8} alt='img8' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img9} alt='img9' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img10} alt='img10' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img11} alt='img11' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img12} alt='img12' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img13} alt='img13' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img14} alt='img14' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img15} alt='img15' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img16} alt='img16' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img17} alt='img17' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img18} alt='img18' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img19} alt='img19' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img20} alt='img20' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img21} alt='img21' className='slide' />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
--- src/pages/Naukaschools.js
+++ src/pages/Naukaschools.js
@@ -1,11 +1,64 @@
 import React from 'react';
+// import Swiper core and required modules
+import { Navigation, Pagination, A11y, Autoplay } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+
+import img1 from '../img/Naukaschools/Викторович.jpg';
+import img2 from '../img/Naukaschools/Лексин 2.jpg';
+import img3 from '../img/Naukaschools/Мазуров 2.jpg';
+import img4 from '../img/Naukaschools/Педагогика и психология.jpg';
+import img5 from '../img/Naukaschools/Прокудин.jpg';
+import img6 from '../img/Naukaschools/Рязанцев.jpg';
+import img7 from '../img/Naukaschools/Экономика Столярова.jpg';
 
 function Naukaschools() {
   return (
-    <div className='main-page main-container page-titles'>
-      <a href='https://disk.yandex.ru/d/tMRhy07KYwlWjw/11.03.22/%D0%9D%D0%B0%D1%83%D1%87%D0%BD%D1%8B%D0%B5%20%D1%88%D0%BA%D0%BE%D0%BB%D1%8B'>
-        Научные школы
-      </a>
+    <div className='main-page'>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Autoplay, A11y]}
+        loop={true}
+        slidesPerView={1}
+        speed={1000}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: true,
+        }}
+        spaceBetween={30}
+        navigation={true}
+        pagination={{
+          type: 'fraction',
+        }}
+      >
+        <SwiperSlide>
+          <img src={img1} alt='img1' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img2} alt='img2' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img3} alt='img3' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img4} alt='img4' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img5} alt='img5' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img6} alt='img6' className='slide' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img7} alt='img7' className='slide' />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
