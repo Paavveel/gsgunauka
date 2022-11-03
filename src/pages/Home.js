67d3e475b6f1954f9f86a9e342d83204a94eec00
Paavveel <pa.tomilin@gmail.com>
2022-11-03 11:02:59 +0300

    feat: add new main images and optimize render

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,52 @@
 import React from 'react';
 // import Swiper core and required modules
-import { Navigation, Pagination, A11y, Autoplay } from 'swiper';
+import { A11y, Autoplay, Navigation, Pagination } from 'swiper';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
+import 'swiper/css/autoplay';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import 'swiper/css/autoplay';
 
-import mainnauka1 from '../img/main-nauka-1.jpg';
-import mainnauka2 from '../img/main-nauka-2.jpg';
-import mainnauka3 from '../img/main-nauka-3.jpg';
-import mainnauka4 from '../img/main-nauka-4.jpg';
-import mainnauka5 from '../img/main-nauka-5.jpg';
-import mainnauka6 from '../img/main-nauka-6.jpg';
-import mainnauka7 from '../img/main-nauka-7.jpg';
-import mainnauka8 from '../img/main-nauka-8.jpg';
+import main1 from '../img/main/1.jpg';
+import main10 from '../img/main/10.jpg';
+import main11 from '../img/main/11.jpg';
+import main12 from '../img/main/12.jpg';
+import main13 from '../img/main/13.jpg';
+import main14 from '../img/main/14.jpg';
+import main15 from '../img/main/15.jpg';
+import main16 from '../img/main/16.jpg';
+import main17 from '../img/main/17.jpg';
+import main2 from '../img/main/2.jpg';
+import main3 from '../img/main/3.jpg';
+import main4 from '../img/main/4.jpg';
+import main5 from '../img/main/5.jpg';
+import main6 from '../img/main/6.jpg';
+import main7 from '../img/main/7.jpg';
+import main8 from '../img/main/8.jpg';
+import main9 from '../img/main/9.jpg';
+
+const images = [
+  main1,
+  main2,
+  main3,
+  main4,
+  main5,
+  main6,
+  main7,
+  main8,
+  main9,
+  main10,
+  main11,
+  main12,
+  main13,
+  main14,
+  main15,
+  main16,
+  main17,
+];
 
 function Home() {
   return (
@@ -38,30 +67,11 @@ function Home() {
           type: 'fraction',
         }}
       >
-        <SwiperSlide>
-          <img src={mainnauka1} alt='mainnauka1' className='slide' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={mainnauka2} alt='mainnauka2' className='slide' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={mainnauka3} alt='mainnauka3' className='slide' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={mainnauka4} alt='mainnauka4' className='slide' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={mainnauka5} alt='mainnauka5' className='slide' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={mainnauka6} alt='mainnauka6' className='slide' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={mainnauka7} alt='mainnauka7' className='slide' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={mainnauka8} alt='mainnauka8' className='slide' />
-        </SwiperSlide>
+        {images?.map((img, i) => (
+          <SwiperSlide>
+            <img src={img} alt={`main-img-${i + 1}`} className='slide' />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
