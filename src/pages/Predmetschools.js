d72a75063f506257affc6c13059044b4189ce298
Paavveel <pa.tomilin@gmail.com>
2023-01-20 08:56:03 +0300

    feat: change and add Predmet schools

--- src/pages/Predmetschools.js
+++ src/pages/Predmetschools.js
@@ -8,42 +8,45 @@ function Predmetschools() {
       <div className='predmetschool-container'>
         <img src={leftsideimg} alt='Предметные школы' />
         <div className='predmetschool-container__right'>
-          <a href='https://disk.yandex.ru/i/ggGnbNtQiPIoqQ'>
+          <a href='https://disk.yandex.ru/i/PX_k59auPNVFLA'>
             Английский без границ
           </a>
-          <a href='https://disk.yandex.ru/i/jmppNipPSRZ6WQ'>
+          <a href='https://disk.yandex.ru/i/EuQ0BZEupt-Krg'>
             Педагогическая студия Я в мире детей
           </a>
-          <a href='https://disk.yandex.ru/i/WrP_vLmDB9qmQA'>
+          <a href='https://disk.yandex.ru/i/hi0VpEfAGtVuCQ'>
             Школа Основы графической грамоты
           </a>
-          <a href='https://disk.yandex.ru/i/Gbpz--0wtG8DBA'>Школа вокала</a>
-          <a href='https://disk.yandex.ru/i/LQcTuBWDahmQ_A'>
-            Школа молодого профессионала
-          </a>
-          <a href='https://disk.yandex.ru/i/whvfxgmMu3syWw'>
+          <a href='https://disk.yandex.ru/i/Mff66swXSWmt2g'>Школа вокала</a>
+          <a href='https://disk.yandex.ru/i/nbw2-7iZSm-Kjw'>
             Школа юного исследователя-химика
           </a>
-          <a href='https://disk.yandex.ru/i/_S9yvjyztW5Trw'>
+          <a href='https://disk.yandex.ru/i/e-S3R8Ajwfmm_g'>
             Школа юного историка-обществоведа
           </a>
-          <a href='https://disk.yandex.ru/i/5RyTgDuAq32Mgw'>
+          <a href='https://disk.yandex.ru/i/LYz9bBrnT2fL7g'>
             Школа юного лингвиста-переводчика
           </a>
-          <a href='https://disk.yandex.ru/i/oz0oZgFoF9XSuw'>
+          <a href='https://disk.yandex.ru/i/pbMZUry3bt19Sw'>
             Школа юного лингвокраеведа
           </a>
-          <a href='https://disk.yandex.ru/i/qJPBE6ijHeSk1w'>
-            Школа юного ученого 3D моделиста
+          <a href='https://disk.yandex.ru/i/vDY2b0FnpJ7plg'>
+            Проектная школа юного 3D моделиста от реального к виртуальному
           </a>
-          <a href='https://disk.yandex.ru/i/eHUf3Ob47RbLSA'>
-            Школа юного ученого (математика, физика, информатика)
-          </a>
-          <a href='https://disk.yandex.ru/i/_0WDJlaFL1wORg'>
+          <a href='https://disk.yandex.ru/i/OL0EWUGy1XDxHA'>
             Школа юного экономиста
           </a>
-          <a href='https://disk.yandex.ru/i/_1AIeCSxka29cA'>
+          <a href='https://disk.yandex.ru/i/PPiYUGxwZcUZ0Q'>
             Профмастерская магистрантов
+          </a>
+          <a href='https://disk.yandex.ru/i/LoPHoqh9lRuoag'>
+            Творческая мастерская Палитра
+          </a>
+          <a href='https://disk.yandex.ru/i/s5MLbRIuUIXa-A'>
+            Школа юного гимнаста
+          </a>
+          <a href='https://disk.yandex.ru/i/aAOiu6KJvn7NEA'>
+            Школа юного ученого Данко
           </a>
         </div>
       </div>
